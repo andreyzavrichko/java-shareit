d673b465f120fd6b7f1f7c0fda5e42b75094a097
a.zavrichko <andrey@zavrichko.dev>
2025-10-31 17:22:37 +0200

    фикс чек стайла

--- src/main/java/ru/practicum/shareit/config/HeaderConstants.java
+++ src/main/java/ru/practicum/shareit/config/HeaderConstants.java
@@ -1,7 +1,9 @@
 package ru.practicum.shareit.config;
 
 public final class HeaderConstants {
-    private HeaderConstants() {}
+
+    private HeaderConstants() {
+    }
 
     public static final String X_SHARER_USER_ID = "X-Sharer-User-Id";
 }
